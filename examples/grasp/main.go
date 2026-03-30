9c700b076812dab286ca2e69955b3c24825feb28
万锴 <wankai20@300624.cn>
2026-03-30 14:59:08 +0800

    feat: 重构下载模块以支持新的 HTTP 客户端和状态管理，优化任务执行和元数据处理

--- examples/grasp/main.go
+++ examples/grasp/main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"github.com/KKKKKKKEM/flowkit/core"
+	"log"
+
 	"github.com/KKKKKKKEM/flowkit/x/grasp"
 )
 
 func main() {
 	p := grasp.NewGraspPipeline()
 
-	//err := p.CLI()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	err := p.CLI()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//if err := p.Serve(":8080"); err != nil {
-	//	log.Fatal(err)
-	//}
+	if err := p.Serve(":8080"); err != nil {
+		log.Fatal(err)
+	}
 
-	p.Invoke(core.NewContext(nil, ""), &grasp.Task{
-		URLs: []string{"https://api.pexels.com/v1/photos/1000"},
-	})
 }
